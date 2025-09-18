f04785f24d379bd3ff1da11130546fa1c6e5146d
Alexandr Shcherbatyuk <a.shcherbatyuk@gmail.com>
2025-09-18 22:30:31 +0300

    more tests

--- test/RebaseToken.t.sol
+++ test/RebaseToken.t.sol
@@ -7,6 +7,7 @@ import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+import {MockContract} from "./Mocks/MockContract.sol";
 
 contract RebaseTokenTest is Test {
     RebaseToken private rebaseToken;
@@ -25,6 +26,9 @@ contract RebaseTokenTest is Test {
         // assertTrue(success);
         vm.stopPrank();
     }
+    /*//////////////////////////////////////////////////////////////
+                                REWARDS
+    //////////////////////////////////////////////////////////////*/
 
     function addRewardsToVault(uint256 amount) public {
         // send some rewards to the vault using the receive function
@@ -34,7 +38,11 @@ contract RebaseTokenTest is Test {
         }
     }
 
-    function testDepositLinear(uint256 amount) public {
+    /*//////////////////////////////////////////////////////////////
+                                DEPOSIT
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzDepositLinear(uint256 amount) public {
         amount = bound(amount, 1e5, type(uint96).max);
         // 1. Deposit
         vm.startPrank(user);
@@ -56,7 +64,26 @@ contract RebaseTokenTest is Test {
         vm.stopPrank();
     }
 
-    function testRedeemStraightAway(uint256 amount) public {
+    /*//////////////////////////////////////////////////////////////
+                                 REDEEM
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzRedeemFails(uint256 amount) public {
+        amount = 1;
+        address mockContract = address(new MockContract());
+
+        // 1. deposit
+        vm.startPrank(mockContract);
+        vm.deal(mockContract, amount);
+        vault.deposit{value: amount}();
+        assertEq(rebaseToken.balanceOf(mockContract), amount);
+        // 2. redeem
+        vm.expectRevert(Vault.Vault__RedeemFailed.selector);
+        vault.redeem(type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function testFuzzRedeemStraightAway(uint256 amount) public {
         amount = bound(amount, 1e5, type(uint96).max);
         // 1. deposit
         vm.startPrank(user);
@@ -70,7 +97,7 @@ contract RebaseTokenTest is Test {
         vm.stopPrank();
     }
 
-    function testRedeemAfterTimePassed(uint256 depositAmount, uint256 time) public {
+    function testFuzzRedeemAfterTimePassed(uint256 depositAmount, uint256 time) public {
         time = bound(time, 1000, type(uint96).max);
         depositAmount = bound(depositAmount, 1e5, type(uint96).max);
         // 1. deposit
@@ -93,7 +120,11 @@ contract RebaseTokenTest is Test {
         assertGt(ethBalance, depositAmount);
     }
 
-    function testTransfer(uint256 amount, uint256 amountToSend) public {
+    /*//////////////////////////////////////////////////////////////
+                                TRANSFER
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzTransfer(uint256 amount, uint256 amountToSend) public {
         amount = bound(amount, 2e5, type(uint96).max);
         amountToSend = bound(amountToSend, 1e5, amount - 1e5);
 
@@ -125,22 +156,152 @@ contract RebaseTokenTest is Test {
         assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
     }
 
-    function testCannotSetInterestRate(uint256 newInterestRate) public {
+    function testFuzzTransferAllTokens(uint256 amount) public {
+        amount = bound(amount, 2e5, type(uint96).max);
+        uint256 amountToSend = type(uint256).max;
+
+        // 1. deposit
+        vm.deal(user, amount);
+        vm.prank(user);
+        vault.deposit{value: amount}();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        // 2. owner reduces the interest rate
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10);
+
+        // 3. transfer
+        vm.prank(user);
+        rebaseToken.transfer(user2, amountToSend);
+        // uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+        assertEq(userBalance + user2Balance, user2BalanceAfterTransfer);
+
+        // check user interast rate has been inherited (5e10 not 4e10)
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+    }
+    /*//////////////////////////////////////////////////////////////
+                             TRANSFER FROM
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzTransferFrom(uint256 amount, uint256 amountToSend) public {
+        amount = bound(amount, 2e5, type(uint96).max);
+        amountToSend = bound(amountToSend, 1e5, amount - 1e5);
+        address spender = makeAddr("spender");
+        // 1. deposit
+        vm.deal(user, amount);
+        vm.startPrank(user);
+        vault.deposit{value: amount}();
+        rebaseToken.approve(spender, amountToSend);
+        vm.stopPrank();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        // 2. owner reduces the interest rate
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10);
+
+        // 3. transfer
+        vm.prank(spender);
+        rebaseToken.transferFrom(user, user2, amountToSend);
+        uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+        assertEq(userBalanceAfterTransfer, userBalance - amountToSend);
+        assertEq(user2BalanceAfterTransfer, user2Balance + amountToSend);
+
+        // check user interast rate has been inherited (5e10 not 4e10)
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+    }
+
+    function testFuzzTransferFromAllTokens(uint256 amount) public {
+        amount = bound(amount, 2e5, type(uint96).max);
+        uint256 amountToSend = type(uint256).max;
+        address spender = makeAddr("spender");
+        // 1. deposit
+        vm.deal(user, amount);
+        vm.startPrank(user);
+        vault.deposit{value: amount}();
+        rebaseToken.approve(spender, amountToSend);
+        vm.stopPrank();
+
+        address user2 = makeAddr("user2");
+        uint256 userBalance = rebaseToken.balanceOf(user);
+        uint256 user2Balance = rebaseToken.balanceOf(user2);
+        assertEq(userBalance, amount);
+        assertEq(user2Balance, 0);
+
+        // 2. owner reduces the interest rate
+        vm.prank(owner);
+        rebaseToken.setInterestRate(4e10);
+
+        // 3. transfer
+        vm.prank(spender);
+        rebaseToken.transferFrom(user, user2, amountToSend);
+        // uint256 userBalanceAfterTransfer = rebaseToken.balanceOf(user);
+        uint256 user2BalanceAfterTransfer = rebaseToken.balanceOf(user2);
+        assertEq(userBalance + user2Balance, user2BalanceAfterTransfer);
+        // assertEq(user2BalanceAfterTransfer, user2Balance + amountToSend);
+
+        // check user interast rate has been inherited (5e10 not 4e10)
+        assertEq(rebaseToken.getUserInterestRate(user), 5e10);
+        assertEq(rebaseToken.getUserInterestRate(user2), 5e10);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                         TRANSFER INTERESTRATE
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzCannotSetInterestRate(uint256 newInterestRate) public {
         vm.prank(user);
         vm.expectPartialRevert(Ownable.OwnableUnauthorizedAccount.selector);
         rebaseToken.setInterestRate(newInterestRate);
     }
 
+    function testGetInterestRateDoNotRevert() public view {
+        uint256 interestRate = rebaseToken.getInterestRate();
+        assertGt(interestRate, 0);
+    }
+
+    function testFuzzInterestRateCanOnlyDecrease(uint256 newInterestRate) public {
+        uint256 initialInterestRate = rebaseToken.getInterestRate();
+        newInterestRate = bound(newInterestRate, initialInterestRate, type(uint96).max);
+        vm.prank(owner);
+        vm.expectPartialRevert(RebaseToken.RebaseToken__InterestRateCanOnlyDecrease.selector);
+        rebaseToken.setInterestRate(newInterestRate);
+        assertEq(rebaseToken.getInterestRate(), initialInterestRate);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             MINT AND BURN
+    //////////////////////////////////////////////////////////////*/
+
     function testCannotCallMintAndBurn() public {
         vm.startPrank(user);
+        uint256 interestRate = rebaseToken.getInterestRate();
+        console.log("interest rate: ", interestRate);
         vm.expectPartialRevert(IAccessControl.AccessControlUnauthorizedAccount.selector);
-        rebaseToken.mint(user, 100, rebaseToken.getInterestRate());
+        rebaseToken.mint(user, 100, interestRate);
         vm.expectPartialRevert(IAccessControl.AccessControlUnauthorizedAccount.selector);
         rebaseToken.burn(user, type(uint256).max);
         vm.stopPrank();
     }
 
-    function testGetPrincipalAmount(uint256 amount) public {
+    /*//////////////////////////////////////////////////////////////
+                             PRINCIPAL BALANCE
+    //////////////////////////////////////////////////////////////*/
+
+    function testFuzzGetPrincipalAmount(uint256 amount) public {
         amount = bound(amount, 1e5, type(uint96).max);
 
         // 1. deposit
@@ -150,16 +311,11 @@ contract RebaseTokenTest is Test {
         assertEq(rebaseToken.principalBalanceOf(user), amount);
     }
 
+    /*//////////////////////////////////////////////////////////////
+                             REBASE TOKEN ADDRESS
+    //////////////////////////////////////////////////////////////*/
+
     function testGetRebaseTokenAddress() public view {
         assertEq(vault.getRebaseTokenAddress(), address(rebaseToken));
-    }
-
-    function testInternalRateCanOnlyDecrease(uint256 newInterestRate) public {
-        uint256 initialInterestRate = rebaseToken.getInterestRate();
-        newInterestRate = bound(newInterestRate, initialInterestRate, type(uint96).max);
-        vm.prank(owner);
-        vm.expectPartialRevert(RebaseToken.RebaseToken__InterestRateCanOnlyDecrease.selector);
-        rebaseToken.setInterestRate(newInterestRate);
-        assertEq(rebaseToken.getInterestRate(), initialInterestRate);
     }
 }
